3c85fb58bd21e24bea802387d03bad998fd1c8f4
Luke C. Pilling <luke.pilling@gmail.com>
2022-12-20 14:29:39 +0000

    Update tidy_ci.R

--- tidy_ci.R
+++ tidy_ci.R
@@ -2,10 +2,10 @@
 ## Luke Pilling -- 2022.12.07
 
 ## By default the (amazing) `broom` package uses the `confint()` function to calculate CIs. For GLMs this calculates confidence intervals via profile likelihood by default.
-## For large datasets (i.e., UK Biobank) this takes a long time and does not meaningfully (?) alter the CIs compared to simply calculating via the SEs.
-## This wrapper function `tidy_ci()` runs `broom::tidy()` and returns the result, plus CIs calculated as 95%/SEs. Much quicker
-## Does the exponentiate function after CI calc 
-## Also exclude intercept for tidier output - can be included
+## For large datasets this takes a long time and does not meaningfully (?) alter the CIs compared to simply calculating using 1.96*SE.
+## This function `tidy_ci()` runs `broom::tidy()` and returns the tidy estimates with CIs calculated as EST +/- 1.96*SE
+##  - Exponentiates estimate and CIs (after CI calculation)
+##  - Also excludse intercept by default for tidier output - can be included at users request
 
 require(broom)
 
