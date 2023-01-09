4998847d0861752af04cfcbf6ff717bc5eafdd9d
Luke C. Pilling <luke.pilling@gmail.com>
2023-01-09 12:18:45 +0000

    Update tidy_ci.R

--- tidy_ci.R
+++ tidy_ci.R
@@ -1,15 +1,20 @@
 
-## Luke Pilling -- 2022.12.20
+## Luke Pilling -- v0.20230109
 
 # Function to run `broom::tidy()` and calculate CIs
 
 # By default the (amazing) `broom` package uses the `confint()` function to calculate CIs. For GLMs this calculates confidence intervals via profile likelihood by default. When using large datasets this takes a long time and does not meaningfully alter the CIs compared to simply calculating using 1.96*SE
 
 # This function `tidy_ci()` runs `broom::tidy()` and returns the tidy estimates with CIs calculated as EST +/- 1.96*SE
-#  - Excludes intercept by default for tidier output - can be included at users request
-#  - Provides negative log10 p-values (if input is class `glm` or `coxph` -- user can provide sample size `n=#` to override)
-#  - If `exp=TRUE` then estimate and CIs are exponentiated after CI calculation
-#  - Other `tidy()` options can be passed
+
+# Options:
+#  `ci` {default=TRUE} calculate CIs using 1.96*SE method
+#  `intercept` {default=FALSE} Exclude intercept for tidier output
+#  `neglog10p` {default=TRUE} Provides negative log10 p-values (if input is class `glm` or `coxph` -- user can provide sample size `n=#` to override)
+#  `exp` {default=FALSE} exponentiate estimate and CIs -- also see `check_family`
+#  `check_family` {default=TRUE} set `exp=TRUE` i.e., exponentiate est/CIs if `glm(family=binomial)` or `coxph()` was performed
+#  `n` {default=NA} the N for `neglog10p` is extracted automatically for `glm` or `coxph` objects - override here if required
+#  `...` Other `tidy()` options 
 
 # Not tested for models other than `glm()` and `survival::coxph()` where it seems to work very well and produces consistent CIs.
 
@@ -23,10 +28,11 @@
 #fit_linear |> tidy_ci()
 
 #fit_logistic = glm(current_smoker_vs_never ~ age + sex + bmi, data = d, family = binomial(link="logit"))
-#fit_logistic |> tidy_ci(exp = TRUE)
+#fit_logistic |> tidy_ci()   # detect model and exponentiate automatically
+#fit_logistic |> tidy_ci(check_family=FALSE)  # override auto checking to get untransformed estimates
 
 #fit_coxph = coxph(Surv(time_to_event, diagnosis_bin) ~ age + sex + bmi + as.factor(smoking_status), data = d)
-#fit_coxph |> tidy_ci(exp = TRUE)
+#fit_coxph |> tidy_ci()
 
 require(dplyr)
 require(broom)
@@ -36,16 +42,17 @@ tidy_ci = function(x = stop("Provide a model fit object"),
 		   exp = FALSE, 
 		   intercept = FALSE, 
 		   neglog10p = TRUE, 
+		   check_family = TRUE,
 		   n = NA, 
 		   ...) {
 	
 	## get tidy output -- do not use `broom` CIs or Exponentiate options
 	ret = broom::tidy(x, conf.int = FALSE, exponentiate = FALSE, ...)
 	
-	## get CIs based on 1.96*SE
+	## get CIs based on 1.96*SE?
 	if (ci)  ret = ret |> dplyr::mutate(conf.low=estimate-(1.96*std.error), conf.high=estimate+(1.96*std.error))
 	
-	## get -log10 p-value
+	## get -log10 p-value?
 	if (neglog10p)  {
 		if (is.na(n) & "glm" %in% class(x))  n = length(x$y)
 		if (is.na(n) & "coxph" %in% class(x))  n = x$n
@@ -53,8 +60,13 @@ tidy_ci = function(x = stop("Provide a model fit object"),
 		if (!is.na(n)) ret = ret |> dplyr::mutate(neglog10p=-1*(pt(abs(estimate/std.error),df=!!n,lower.tail=F,log.p=T) + log(2))/log(10))
 	}
 	
-	## exponentiate if required
-	if (exp) ret = ret |> dplyr::mutate(estimate=exp(estimate), conf.low=exp(conf.low), conf.high=exp(conf.high))
+	## exponentiate estimate and CIs?
+	if (check_family)  {
+		if ("glm" %in% class(x)) if (x$family$family == "binomial") exp = TRUE 
+		if ("coxph" %in% class(x)) exp = TRUE 
+	}
+	if (exp) ret = ret |> dplyr::mutate(estimate=exp(estimate))
+	if (exp & ci) ret = ret |> dplyr::mutate(conf.low=exp(conf.low), conf.high=exp(conf.high))
 	
 	## exclude intercept?
 	if (!intercept) ret = ret |> dplyr::filter(term!="(Intercept)")
